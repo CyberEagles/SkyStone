8e298d8be716d0b8fb18e21168daf83ceac8d846
AzureAD\JacobZarowny <j.zarowny19@gmail.com>
2019-10-24 17:50:52 -0600

    Odometer now goes in the right direction.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerTesting2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerTesting2.java
@@ -53,11 +53,11 @@ public class OdometerTesting2 extends LinearOpMode {
     private ElapsedTime     runtime = new ElapsedTime();
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.00;     // This is < 1.0 if geared UP //0.75 for chassis, x for susan
+    static final double     DRIVE_GEAR_REDUCTION    = 0.147;     // This is < 1.0 if geared UP //0.75 for chassis, x for susan
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.5 ;
+    static final double     DRIVE_SPEED             = 0.75 ;
     static final double     TURN_SPEED              = 0.5;
 
     @Override
@@ -106,7 +106,7 @@ public class OdometerTesting2 extends LinearOpMode {
         telemetry.update();
 
 
-        DriveBackwards(DRIVE_SPEED,12,5);
+        DriveBackwards(DRIVE_SPEED,-12,5);
 
 
 
@@ -141,9 +141,9 @@ public class OdometerTesting2 extends LinearOpMode {
             robot.leftFrontDrive.setTargetPosition(newLeftFrontTarget);
 
             // Turn On RUN_TO_POSITION
-            robot.leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
 
@@ -158,7 +158,7 @@ public class OdometerTesting2 extends LinearOpMode {
 
             while (opModeIsActive() &&
                     (runtime.seconds() < timeoutS) &&
-                    (robot.leftBackDrive.isBusy() )) { // && robot.rightFrontDrive.isBusy() && robot.leftBackDrive.isBusy() && robot.rightBackDrive.isBusy())) {
+                    (robot.leftFrontDrive.isBusy() )) { // && robot.rightFrontDrive.isBusy() && robot.leftBackDrive.isBusy() && robot.rightBackDrive.isBusy())) {
 
 
             }
