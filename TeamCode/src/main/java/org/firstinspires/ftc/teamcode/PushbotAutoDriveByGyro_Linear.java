9ebf7eb36cdb7858a8d99f7995f0e411a9c412d1
AzureAD\JacobZarowny <j.zarowny19@gmail.com>
2019-11-07 17:57:23 -0700

    Trying to make gyro turn more relaible be careful because the motors were doing a weird clicky thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByGyro_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByGyro_Linear.java
@@ -42,9 +42,9 @@ import org.firstinspires.ftc.teamcode.HardwarePushbot;
         // These constants define the desired driving/control characteristics
         // The can/should be tweaked to suite the specific robot drive train.
         static final double     DRIVE_SPEED             = 1;     // Nominal speed for better accuracy.
-        static final double     TURN_SPEED              = 0.5;     // Nominal half speed for better accuracy.
+        static final double     TURN_SPEED              = 0.7;     // Nominal half speed for better accuracy.
 
-        static final double     HEADING_THRESHOLD       = 1 ;      // As tight as we can make it with an integer gyro
+        static final double     HEADING_THRESHOLD       = 3 ;      // As tight as we can make it with an integer gyro
         static final double     P_TURN_COEFF            = 1.0;     // Larger is more responsive, but also less stable
         static final double     P_DRIVE_COEFF           = 0.1;     // Larger is more responsive, but also less stable
 
@@ -106,13 +106,12 @@ import org.firstinspires.ftc.teamcode.HardwarePushbot;
             // Note: Reverse movement is obtained by setting a negative distance (not speed)
             // Put a hold after each turn
 //wait for start?
-            gyroDrive(DRIVE_SPEED, 36, 0);
-     //       gyroTurn( TURN_SPEED,   90.0);
-            sleep(5000000);
-           //gyroDrive(DRIVE_SPEED, 36, 0);
+//            gyroDrive(DRIVE_SPEED, 1, 0);
+        gyroTurn( TURN_SPEED,   45.0);
+//           gyroDrive(DRIVE_SPEED, 36, 0);
             //gyroHold(TURN_SPEED, 90, 2);
            // gyroTurn( TURN_SPEED,   -90.0);
-
+            sleep(5000000);
 
 //            telemetry.addData("Path", "Complete");
 //////            telemetry.update();
@@ -250,10 +249,11 @@ import org.firstinspires.ftc.teamcode.HardwarePushbot;
             while (opModeIsActive() && !onHeading(speed, angle, P_TURN_COEFF)) {
                 // Update telemetry & Allow time for other processes to run.
                 telemetry.update();
-                //robot.leftFrontDrive.setPower(-1);
-                //robot.rightFrontDrive.setPower(1);
-                //robot.rightBackDrive.setPower(1);
-                //robot.leftBackDrive.setPower(-1);
+                telemetry.addData("motors turning on",0);
+                robot.leftFrontDrive.setPower(-TURN_SPEED);
+                robot.rightFrontDrive.setPower(TURN_SPEED);
+                robot.rightBackDrive.setPower(TURN_SPEED);
+                robot.leftBackDrive.setPower(-TURN_SPEED);
             }
             robot.leftFrontDrive.setPower(0);
             robot.rightFrontDrive.setPower(0);
