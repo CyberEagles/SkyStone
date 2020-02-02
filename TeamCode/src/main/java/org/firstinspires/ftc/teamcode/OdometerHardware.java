001130e527c89801e1d292640f890433d3b1dc28
ben <bloewen999@gmail.com>
2020-02-02 16:01:23 -0700

    Add foundation servo.  Add turn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerHardware.java
@@ -22,9 +22,13 @@ public class OdometerHardware {
     public OdometerHardware (LinearOpMode opMode){
         this.opMode= opMode;}
     DcMotor rightFrontDrive, rightBackDrive, leftFrontDrive, leftBackDrive, skystoneGrabber, craneMotor;
-    Servo claw;
     //Odometry Wheels
     DcMotor verticalLeft, verticalRight, horizontal;
+
+    public Servo clamp = null;             //grabbing stones
+    public Servo claw = null;              //skystone Grabber pinch servo, Part 2
+    public CRServo stoneRotator = null;    //once stone is grabbed move outside robot
+    public Servo foundation = null;
 
     final double COUNTS_PER_INCH = 307.699557;
     final int FORWARD = 1;
@@ -52,6 +56,90 @@ public class OdometerHardware {
 
 //        globalPositionUpdate.reverseLeftEncoder();
 
+    public void turn (double robotPower, double desiredRobotOrientation, double allowableAngleError, double timeoutS) {
+        runtime.reset();
+
+        double orientationAngle = globalPositionUpdate.returnOrientation();
+
+        if (orientationAngle < 0){
+            orientationAngle += 360;
+        }
+
+        double angleDifference = desiredRobotOrientation - orientationAngle;
+
+        while (angleDifference > 180)  angleDifference -= 360;
+        while (angleDifference <= -180) angleDifference += 360;
+
+        while (opMode.opModeIsActive() && Math.abs(angleDifference) > allowableAngleError && (runtime.seconds() < timeoutS)) {
+
+
+
+            orientationAngle = globalPositionUpdate.returnOrientation();
+
+            if (orientationAngle < 0){
+                orientationAngle += 360;
+            }
+
+            angleDifference = desiredRobotOrientation - orientationAngle;
+
+            while (angleDifference > 180)  angleDifference -= 360;
+            while (angleDifference <= -180) angleDifference += 360;
+
+            if (Math.abs(angleDifference) > allowableAngleError) {
+
+                if ( angleDifference > 0) {
+                    //turn right
+                    leftFrontDrive.setPower(robotPower);
+                    rightFrontDrive.setPower(robotPower);
+                    leftBackDrive.setPower(robotPower);
+                    rightBackDrive.setPower(robotPower);
+
+                    opMode.telemetry.addData("Turning right", "now");
+                    opMode.telemetry.addData("X Position (inches)", globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH);
+                    opMode.telemetry.addData("Y Position (inches)", globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH);
+                    opMode.telemetry.addData("Orientation (Degrees)", orientationAngle);
+
+                    opMode.telemetry.addData("Vertical left encoder position", verticalLeft.getCurrentPosition());
+                    opMode.telemetry.addData("Vertical right encoder position", verticalRight.getCurrentPosition());
+                    opMode.telemetry.addData("horizontal encoder position", horizontal.getCurrentPosition());
+
+                    opMode.telemetry.update();
+
+                }
+                else if (angleDifference< 0) {
+                    leftFrontDrive.setPower(-robotPower);
+                    rightFrontDrive.setPower(-robotPower);
+                    leftBackDrive.setPower(-robotPower);
+                    rightBackDrive.setPower(-robotPower);
+
+
+                    opMode. telemetry.addData("Turning left", "now");
+                    opMode. telemetry.addData("X Position (inches)", globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH);
+                    opMode.telemetry.addData("Y Position (inches)", globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH);
+                    opMode.telemetry.addData("Orientation (Degrees)", orientationAngle);
+
+
+                    opMode.telemetry.addData("Vertical left encoder position", verticalLeft.getCurrentPosition());
+                    opMode. telemetry.addData("Vertical right encoder position", verticalRight.getCurrentPosition());
+                    opMode. telemetry.addData("horizontal encoder position", horizontal.getCurrentPosition());
+
+                    opMode.telemetry.update();
+
+                    //turn left
+                }
+
+            }
+
+
+
+        }
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+
+    }
 
 
 
@@ -60,6 +148,106 @@ public class OdometerHardware {
     public void goToPosition(double targetXPosition, double targetYPosition, double robotPower, double desiredRobotOrientation, double allowableDistanceError, double timeoutS) {
         goToPosition(targetXPosition, targetYPosition, robotPower, desiredRobotOrientation, allowableDistanceError, timeoutS, FORWARD);
     }
+    // To fix Strafe create second method
+
+    public void initDriveHardwareMap(){
+        String rfName = "right_front", rbName = "right_back", lfName = "left_front", lbName = "left_back";
+        String vlEncoderName = rfName, vrEncoderName = lfName, hEncoderName = lbName;
+
+        rightFrontDrive = opMode.hardwareMap.dcMotor.get(rfName);
+        rightBackDrive = opMode.hardwareMap.dcMotor.get(rbName);
+        leftFrontDrive = opMode.hardwareMap.dcMotor.get(lfName);
+        leftBackDrive = opMode.hardwareMap.dcMotor.get(lbName);
+        skystoneGrabber = opMode.hardwareMap.dcMotor.get("skystone");
+        craneMotor = opMode.hardwareMap.get (DcMotor.class, "crane");
+
+        claw = opMode.hardwareMap.servo.get("claw");
+        clamp = opMode.hardwareMap.servo.get("clamp");
+        stoneRotator = opMode.hardwareMap.crservo.get("stone_rotator");
+        foundation = opMode.hardwareMap.servo.get ("foundation");
+
+
+        verticalLeft = opMode.hardwareMap.dcMotor.get(vlEncoderName);
+        verticalRight = opMode.hardwareMap.dcMotor.get(vrEncoderName);
+        horizontal = opMode.hardwareMap.dcMotor.get(hEncoderName);
+
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        craneMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        craneMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        craneMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        craneMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+
+        opMode.telemetry.addData("Status", "Init Complete");
+        opMode.telemetry.update();
+
+
+
+
+        //Create and start GlobalCoordinatePosition thread to constantly update the global coordinate positions
+        globalPositionUpdate = new OdometryGlobalCoordinatePosition(verticalLeft, verticalRight, horizontal, COUNTS_PER_INCH, 75);
+        Thread positionThread = new Thread(globalPositionUpdate);
+        positionThread.start();
+//        globalPositionUpdate.reverseRightEncoder();
+//        globalPositionUpdate.reverseNormalEncoder();
+
+        rightBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        verticalLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        verticalRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        horizontal.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        opMode.telemetry.addData("Status", "Hardware Map Init Complete");
+        opMode.telemetry.update();
+    }
+
+    /**
+     * Calculate the power in the x direction
+     * @param desiredAngle angle on the x axis
+     * @param speed robot's speed
+     * @return the x vector
+     */
+    private double calculateX(double desiredAngle, double speed) {
+        return Math.sin(Math.toRadians(desiredAngle)) * speed;
+    }
+
+    /**
+     * Calculate the power in the y direction
+     * @param desiredAngle angle on the y axis
+     * @param speed robot's speed
+     * @return the y vector
+     */
+    private double calculateY(double desiredAngle, double speed) {
+        return Math.cos(Math.toRadians(desiredAngle)) * speed;
+    }
+
     public void goToPosition(double targetXPosition, double targetYPosition, double robotPower, double desiredRobotOrientation, double allowableDistanceError, double timeoutS, int direction) {
 
         double distanceToXTarget = targetXPosition*COUNTS_PER_INCH - globalPositionUpdate.returnXCoordinate();
@@ -196,102 +384,5 @@ public class OdometerHardware {
         opMode.telemetry.update();
 
 
-    }
-    // To fix Strafe create second method
-
-
-    public void initDriveHardwareMap(){
-        String rfName = "right_front", rbName = "right_back", lfName = "left_front", lbName = "left_back";
-        String vlEncoderName = rfName, vrEncoderName = lfName, hEncoderName = lbName;
-
-        rightFrontDrive = opMode.hardwareMap.dcMotor.get(rfName);
-        rightBackDrive = opMode.hardwareMap.dcMotor.get(rbName);
-        leftFrontDrive = opMode.hardwareMap.dcMotor.get(lfName);
-        leftBackDrive = opMode.hardwareMap.dcMotor.get(lbName);
-        skystoneGrabber = opMode.hardwareMap.dcMotor.get("skystone");
-        craneMotor = opMode.hardwareMap.get (DcMotor.class, "crane");
-
-        claw = opMode.hardwareMap.servo.get("claw");
-
-
-        verticalLeft = opMode.hardwareMap.dcMotor.get(vlEncoderName);
-        verticalRight = opMode.hardwareMap.dcMotor.get(vrEncoderName);
-        horizontal = opMode.hardwareMap.dcMotor.get(hEncoderName);
-
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        craneMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        craneMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
-        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        craneMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        craneMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-
-
-        opMode.telemetry.addData("Status", "Init Complete");
-        opMode.telemetry.update();
-
-
-
-
-        //Create and start GlobalCoordinatePosition thread to constantly update the global coordinate positions
-        globalPositionUpdate = new OdometryGlobalCoordinatePosition(verticalLeft, verticalRight, horizontal, COUNTS_PER_INCH, 75);
-        Thread positionThread = new Thread(globalPositionUpdate);
-        positionThread.start();
-//        globalPositionUpdate.reverseRightEncoder();
-//        globalPositionUpdate.reverseNormalEncoder();
-
-        rightBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        verticalLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        verticalRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        horizontal.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        opMode.telemetry.addData("Status", "Hardware Map Init Complete");
-        opMode.telemetry.update();
-    }
-
-    /**
-     * Calculate the power in the x direction
-     * @param desiredAngle angle on the x axis
-     * @param speed robot's speed
-     * @return the x vector
-     */
-    private double calculateX(double desiredAngle, double speed) {
-        return Math.sin(Math.toRadians(desiredAngle)) * speed;
-    }
-
-    /**
-     * Calculate the power in the y direction
-     * @param desiredAngle angle on the y axis
-     * @param speed robot's speed
-     * @return the y vector
-     */
-    private double calculateY(double desiredAngle, double speed) {
-        return Math.cos(Math.toRadians(desiredAngle)) * speed;
     }
 }
