1f6869a84c31392333a6ef38b17210e6db53fe96
AzureAD\JacobZarowny <j.zarowny19@gmail.com>
2020-02-06 20:59:57 -0700

    Finished the auto so we can grab a stone, put it on the foundation, move the foundation, and park.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStoneGrab.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStoneGrab.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous
 public class RedStoneGrab extends LinearOpMode {
     OdometerHardware robot   = new OdometerHardware(this);
@@ -10,21 +12,45 @@ public class RedStoneGrab extends LinearOpMode {
     public void runOpMode () {
         robot.initDriveHardwareMap();
         waitForStart();
-        robot.goToPosition(0,-26,0.6,0,5,5, robot.BACKWARD);
-        robot.turn(0.5, -75, 3, 5);
-        robot.goToPosition(-3, -26, 0.6, 0, 2, 5);
+        robot.goToPosition(0,-25,0.6,0,2,5, robot.BACKWARD);
         robot.skystoneGrabber.setPower(0.5);
+        robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        sleep(500);
+        robot.skystoneGrabber.setPower(0);
+        robot.turn(0.5, -75, 3, 5);
+        robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.goToPosition(-3, -26, 0.6, 0, 2, 5);
         robot.claw.setPosition(1);
         sleep(2000);
         robot.skystoneGrabber.setPower(-0.5);
         sleep(1000);
         robot.skystoneGrabber.setPower(0);
-        robot.goToPosition(-82, -26, 0.6, 0, 5, 5);
+        robot.goToPosition(-72, -16, 0.8, 0, 2, 5);
+//testing these 3 ^vv
+        robot.goToPosition(-82,-26,0.8,0,2,5);
+        robot.turn(0.5,-90,3,5);
         robot.skystoneGrabber.setPower(0.5);
         sleep(700);
         robot.claw.setPosition(0);
-        sleep(2000);
+        sleep(1500);
         robot.skystoneGrabber.setPower(-0.5);
+//Joel's new added fun stuff
+        sleep(700);
+        robot.skystoneGrabber.setPower(0);
+        robot.claw.setPosition(0.5);
+        robot.turn(0.5,0,3,5);
+        robot.goToPosition(-82,-32,0.6,0,3,5,robot.BACKWARD);
+        robot.foundation.setDirection(Servo.Direction.FORWARD);
+        robot.foundation.setPosition(0);
+        sleep(1000);
+        robot.goToPosition(-72,-10,0.8,0,3,5);
+        robot.turn(0.5,90,3,5);
+        robot.goToPosition(-90,-21,0.8,0,3,2,robot.BACKWARD);
+        robot.foundation.setPosition(180);
+        sleep(500);
+        robot.goToPosition(-35,-23,0.8,0,3,5);
+
+
 
 
         while(opModeIsActive()){
