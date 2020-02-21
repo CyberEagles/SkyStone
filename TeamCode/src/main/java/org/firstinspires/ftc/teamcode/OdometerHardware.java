acfe7f9d460b11c49b51484004e9909fa06b8abe
ben <bloewen999@gmail.com>
2020-02-21 15:59:40 -0700

    Everything works with the odometers, motors, and servos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometerHardware.java
@@ -25,7 +25,7 @@ public class OdometerHardware {
     //Odometry Wheels
     DcMotor verticalLeft, verticalRight, horizontal;
 
-    public Servo clamp = null;             //grabbing stones
+    public CRServo clamp = null;             //grabbing stones
     public Servo claw = null;              //skystone Grabber pinch servo, Part 2
     public CRServo stoneRotator = null;    //once stone is grabbed move outside robot
     public Servo foundation = null;
@@ -162,7 +162,7 @@ public class OdometerHardware {
         craneMotor = opMode.hardwareMap.get (DcMotor.class, "crane");
 
         claw = opMode.hardwareMap.servo.get("claw");
-        clamp = opMode.hardwareMap.servo.get("clamp");
+        clamp = opMode.hardwareMap.crservo.get("clamp");
         stoneRotator = opMode.hardwareMap.crservo.get("stone_rotator");
         foundation = opMode.hardwareMap.servo.get ("foundation");
 
