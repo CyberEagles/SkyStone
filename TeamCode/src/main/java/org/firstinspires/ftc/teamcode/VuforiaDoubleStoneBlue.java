2b02bf1dd4104088fb6794b9f827a4b79016dabd
joelcodez <joelcodesrobots@gmail.com>
2020-03-05 22:49:43 -0700

    These programs are mostly ready for competition. Maybe a little more work on the foundation one though.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaDoubleStoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaDoubleStoneBlue.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.vuforia.CameraDevice;
 
 @Autonomous
-public class VuforiaStoneFoundationBlue extends LinearOpMode {
+public class VuforiaDoubleStoneBlue extends LinearOpMode {
     OdometerHardware robot   = new OdometerHardware(this);
     @Override
 
@@ -84,45 +84,102 @@ public class VuforiaStoneFoundationBlue extends LinearOpMode {
         sleep(1000);
         robot.skystoneGrabber.setPower(0);
 //grab the stone and lift it into robot ^
-        robot.goToPosition(35,-25,1,0,4.5,5,robot.BACKWARD);
-        robot.goToPosition(82,-30,0.8,0,2,5,robot.BACKWARD);
-        robot.turn(0.5,-90,3,5);
-        currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
-        robot.goToPosition(currentX, -36, 0.8, 0, 3, 5, robot.STRAFELEFT);
-//drive to point closer to foundation but close to the wall, then closer to foundation and rotate to line up nicely ^
+        robot.goToPosition(49,-25,1,0,4.5,5,robot.BACKWARD);
+
+//drive to point closer to skybridge ^
         robot.skystoneGrabber.setPower(0.5);
         sleep(700);
-//lower stone grabber with stone onto foundation ^
+//lower stone grabber with stone ^
         robot.claw.setPower(-1);
         sleep(1000);
-//let go of skystone while on foundation ^
+//let go of skystone ^
         robot.skystoneGrabber.setPower(-0.5);
         sleep(700);
         robot.skystoneGrabber.setPower(0);
         robot.claw.setPower(0);
 //lift skystone grabber back into robot ^
-        currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
-        robot.goToPosition(currentX,-30,0.8,0,3,5,robot.STRAFERIGHT);
-        robot.turn(0.8,0,4,5);
-        currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
-        robot.goToPosition(currentX,-38,0.6,0,2,5,robot.BACKWARD);
-//rotate and drive into foundation more ^
-        robot.foundation.setDirection(Servo.Direction.FORWARD);
-        robot.foundation.setPosition(0);
+        robot.goToPosition(15,-25,1,0,4.5,5,robot.FORWARD);
+
+        if (skystonePosition == 1){
+            robot.skystoneGrabber.setPower(0.5);
+            robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            sleep(500);
+            robot.skystoneGrabber.setPower(0);
+//lower skystone grabber ^
+            //this should be duplicated under each if loop, or above the if loop.
+
+            robot.goToPosition(0, -25, 0.6, 0, 1, 5, robot.FORWARD);
+            robot.turn(0.6, -90, 5, 5);
+            currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
+            robot.goToPosition(currentX, -30, 0.8, 0, 3, 5, robot.STRAFELEFT);
+            robot.turn(0.4, -90, 2, 2);
+            //move closer to the stone ^
+
+        }
+        else if (skystonePosition == 3) {
+            telemetry.addData("Skystone Found!", "Left half of screen");
+            telemetry.update();
+
+            robot.skystoneGrabber.setPower(0.5);
+            robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            sleep(500);
+            robot.skystoneGrabber.setPower(0);
+//lower skystone grabber ^
+            //this should be duplicated under each if loop, or above the if loop.
+
+            robot.goToPosition(-32, -25, 1, 0, 3, 5, robot.FORWARD);
+            robot.turn(0.6, -90, 5, 5);
+            currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
+            robot.goToPosition(currentX, -30, 0.8, 0, 3, 5, robot.STRAFELEFT);
+            robot.turn(0.4, -75, 2, 2);
+            //move closer to the stone ^
+        }
+        else {
+            telemetry.addData("Skystone NOT Found!", "going for the third one");
+            telemetry.update();
+
+            robot.skystoneGrabber.setPower(0.5);
+            robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            sleep(500);
+            robot.skystoneGrabber.setPower(0);
+//lower skystone grabber ^
+            //this should be duplicated under each if loop, or above the if loop.
+
+            robot.turn(0.6, -90, 5, 5);
+            robot.goToPosition(-24, -30, 0.8, 0, 3, 5, robot.FORWARD);
+            robot.turn(0.6, -90, 5, 5);
+            currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
+            robot.goToPosition(currentX, -30, 0.8, 0, 3, 5, robot.STRAFELEFT);
+            robot.turn(0.4, -90, 2, 5);
+
+            //move closer to the stone ^
+
+        }
+        robot.claw.setPower(1);
+        sleep(2500);
+        robot.skystoneGrabber.setPower(-0.5);
         sleep(1000);
-//move the servo to grab the foundation
-        currentX = robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH;
-        robot.goToPosition(currentX,-20,1,0,6,5,robot.FORWARD);
-        robot.goToPosition(72,-20,1,0,5,5, robot.FORWARD);
-        robot.turn(0.7,-90,3,5);
-        robot.goToPosition(90,-20,1,0,5,1.0, robot.BACKWARD);
-        robot.foundation.setPosition(180);
-        robot.goToPosition(90,-17,1,0,6,1.0,robot.BACKWARD);
-//Drag foundation, rotate it, and push it into corner. then let go ^
-        robot.goToPosition(35,-27,1,0,2,5);
+        robot.skystoneGrabber.setPower(0);
+//grab the stone and lift it into robot ^
+        robot.goToPosition(45,-25,1,0,4.5,5,robot.BACKWARD);
+
+//drive to point closer to skybridge ^
+        robot.skystoneGrabber.setPower(0.5);
+        sleep(700);
+//lower stone grabber with stone ^
+        robot.claw.setPower(-1);
+        sleep(1000);
+//let go of skystone ^
+        robot.skystoneGrabber.setPower(-0.5);
+        sleep(700);
+        robot.skystoneGrabber.setPower(0);
+        robot.claw.setPower(0);
+//lift skystone grabber back into robot ^
+
+        robot.goToPosition(35,-27,1,0,2,5,robot.FORWARD);
         sleep(5000);
 //Park on the line^
-//
+
 //        robot.goToPosition(0,-25,0.6,0,2,5, robot.BACKWARD);
 //        robot.skystoneGrabber.setPower(0.5);
 //        robot.skystoneGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
